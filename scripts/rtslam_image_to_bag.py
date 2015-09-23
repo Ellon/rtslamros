0d1a86604b477ee384897f5ba88a328b1d4164b1
Ellon Mendes <ellon.mendes@laas.fr>
2015-09-23 16:40:07 +0200

    Add tf transforms between base and cameras

--- scripts/rtslam_image_to_bag.py
+++ scripts/rtslam_image_to_bag.py
@@ -13,7 +13,11 @@ import cv2
 import rospy
 import rosbag
 import numpy
+from math import sin, cos
 from sensor_msgs.msg import Image, CameraInfo
+from tf.msg import tfMessage
+from tf.transformations import quaternion_from_matrix, translation_from_matrix
+from geometry_msgs.msg import TransformStamped
 
 def restart_line():
     sys.stdout.write('\r')
@@ -48,17 +52,58 @@ def get_intrinsic_distortion(filepath='setup.cfg', camera=''):
 
     return intrinsic, distortion
 
+def matrix(yaw, pitch, roll, x, y, z):
+    # from pom-genom/libeuler/pomEuler.c:287 (pomWriteSensorPos)
+    # euler.{yaw,pitch,roll,x,y,z}
+    ca, sa = cos(yaw),   sin(yaw)
+    cb, sb = cos(pitch), sin(pitch)
+    cg, sg = cos(roll),  sin(roll)
+    return [[ ca*cb, ca*sb*sg - sa*cg, ca*sb*cg + sa*sg, x],
+            [ sa*cb, sa*sb*sg + ca*cg, sa*sb*cg - ca*sg, y],
+            [ -sb, cb*sg, cb*cg, z],
+            [ 0.0, 0.0, 0.0, 1.0]]
 
+def tfm(matrix, parent, child, stamp):
+    rotation = quaternion_from_matrix(matrix)
+    translation = translation_from_matrix(matrix)
+    t = TransformStamped()
+    t.header.frame_id = parent
+    t.header.stamp = stamp
+    t.child_frame_id = child
+    t.transform.translation.x = translation[0]
+    t.transform.translation.y = translation[1]
+    t.transform.translation.z = translation[2]
+    t.transform.rotation.x = rotation[0]
+    t.transform.rotation.y = rotation[1]
+    t.transform.rotation.z = rotation[2]
+    t.transform.rotation.w = rotation[3]
+    return tfMessage([t])
+
+def get_camera_pose_as_matrix(filepath='setup.cfg', camera=''):
+    with open(filepath) as f:
+      lines = f.readlines()
+    parser = lambda label: map(float, [line for line in lines \
+        if line.startswith(label)][0].strip().split('(')[1].split(')')[0].split(','))
+    p = parser('CAMERA%s_POSE_INERTIAL:'%camera)
+    x, y, z = p[0:3]
+    roll, pitch, yaw = numpy.deg2rad(p[3:6])
+
+    return matrix(yaw, pitch, roll, x, y, z)
+
+
+# Get program arguments
 dataset_dir = sys.argv[1]
 camera_id = sys.argv[2]
 begin = 0 if len(sys.argv) < 4 else int(sys.argv[3])
 
+# Set names used along the script
 name = 'image' if camera_id == 'mono' \
                else 'image_%i'%int(camera_id)
 camera_name = 'camera' if camera_id == 'mono' \
                else 'camera%i'%int(camera_id)
 template = name+'_%07i'
 
+base_frame = "base"
 
 # Common attributes for image msg
 image = Image()
@@ -87,6 +132,10 @@ camera_info.P = [intrinsic[0][0], intrinsic[0][1], intrinsic[0][2], Tx,
                  intrinsic[1][0], intrinsic[1][1], intrinsic[1][2], Ty,
                  intrinsic[2][0], intrinsic[2][1], intrinsic[2][2], 0]
 
+# Fixed base-to-camera transform
+base_to_camera = get_camera_pose_as_matrix(os.path.join(dataset_dir,'setup.cfg'), camera)
+
+
 with rosbag.Bag('%s.bag'%camera_name, 'w') as bag:
     while 1:
         # Print info
@@ -114,6 +163,7 @@ with rosbag.Bag('%s.bag'%camera_name, 'w') as bag:
        # Write on bag
         bag.write('/%s/image'%camera_name, image, image.header.stamp)
         bag.write('/%s/camera_info'%camera_name, camera_info, camera_info.header.stamp)
+        bag.write('/tf', tfm(base_to_camera, base_frame, camera_name, image.header.stamp), image.header.stamp)
 
         # Advance seq
         image.header.seq += 1
