650ac231524ef3ad087e5c17afb9fd071ba680ee
Pierrick Koch <pierrick.koch@gmail.com>
2015-09-24 10:25:09 +0200

    [scripts] cleanup imu bag

--- scripts/imu_bag.py
+++ scripts/imu_bag.py
@@ -1,41 +1,33 @@
 #!/usr/bin/env python
-'''
-Convert RTSLAM dataset to ROS bag
+"""
+Convert IMU (MTI) log to ROS bag
 
-Usage:
-./rtslam_dataset_to_ros_bag.py <path-to-MTI.log-file> <path-to-image-dir> <bagname>
-'''
-
+usage: imu_bag.py [MTI.log]
+"""
 import sys
 import rospy
 import rosbag
 from sensor_msgs.msg import Image, Imu
 
-mtilog_path = sys.argv[1]
-imagedir_path = sys.argv[2]
-bagname = sys.argv[3]
+filepath = 'MTI.log' if len(sys.argv) < 2 else sys.argv[1]
 
-with open(mtilog_path) as f:
+with open(filepath) as f:
     lines = f.readlines()
 
-mti_data = [map(float, line.split()) for line in lines if not line.startswith('#')]
+data = [map(float, line.split()) for line in lines if not line.startswith('#')]
 
-with rosbag.Bag(bagname, 'w') as bag:
-	imu_seq=1
-    for date, acc_x, acc_y, acc_z, angvel_x, angvel_y, angvel_z, mag_x, mag_y, mag_z in mti_data:
-		imu_msg = Imu()
-		imu_msg.header.stamp = rospy.Time.from_sec(date)
-		imu_msg.header.seq = imu_seq
-		# imu_msg.header.frame_id = ???
-		imu_msg.linear_acceleration.x = acc_x
-		imu_msg.linear_acceleration.y = acc_y
-		imu_msg.linear_acceleration.z = acc_z
-		imu_msg.angular_velocity.x = angvel_x
-		imu_msg.angular_velocity.y = angvel_y
-		imu_msg.angular_velocity.z = angvel_z
-		# Inform we doesn't have orientation estimates
-		# see http://docs.ros.org/api/sensor_msgs/html/msg/Imu.html
-		imu_msg.orientation_covariance[0] = -1
-		bag.write("imu", imu_msg, imu_msg.header.stamp)
-		imu_seq += 1
+imu = Imu()
+imu.header.frame_id = 'imu'
+imu.header.seq = 0
 
+with rosbag.Bag(filepath+'.bag', 'w') as bag:
+    for date, imu.linear_acceleration.x, imu.linear_acceleration.y, \
+            imu.linear_acceleration.z, imu.angular_velocity.x, \
+            imu.angular_velocity.y, imu.angular_velocity.z, \
+            mag_x, mag_y, mag_z in data:
+        imu.header.stamp = rospy.Time.from_sec(date)
+        # Inform we doesn't have orientation estimates
+        # see http://docs.ros.org/api/sensor_msgs/html/msg/Imu.html
+        imu.orientation_covariance[0] = -1
+        bag.write("/imu", imu, imu.header.stamp)
+        imu.header.seq += 1
