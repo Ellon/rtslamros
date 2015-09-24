89797bc76c3a91f6b465b54c664f83ed701a67f4
Pierrick Koch <pierrick.koch@gmail.com>
2015-09-24 12:03:39 +0200

    [scripts] add camera info to yaml
    separate rtslam setup.cfg parsing from ros bag generation

--- scripts/image_bag.py
+++ scripts/image_bag.py
@@ -1,11 +1,12 @@
 #! /usr/bin/env python
 """
 usage:   image_bag.py 1|2|mono [begin]
-example: image_bag.py 1 0
+example: image_bag.py 1
 example: image_bag.py mono 42
 """
 import sys
 import cv2
+import yaml
 import rospy
 import rosbag
 from sensor_msgs.msg import Image, CameraInfo
@@ -16,40 +17,17 @@ def image_time(filepath):
         time = float(f.read())
     return img, time
 
-def get_intrinsic(filepath='setup.cfg', camera=''):
-    with open(filepath) as f:
-        lines = f.readlines()
-    parser = lambda label: map(float, [line for line in lines \
-        if line.startswith(label)][0].strip().split('(')[1].split(')')[0].split(','))
-    i = parser('CAMERA%s_INTRINSIC:'%camera)
-    d = parser('CAMERA%s_DISTORTION:'%camera)
-    # TODO
-    return [[0]*3]*3
-
-
 image = Image()
 image.encoding = "mono8"
 image.header.frame_id = 'image' if sys.argv[1] == 'mono' \
                         else 'image_%i'%int(sys.argv[1])
 image.header.seq = 0 if len(sys.argv) < 3 else int(sys.argv[2])
-camera = '' if sys.argv[1] == 'mono' else sys.argv[1]
-intrinsic = get_intrinsic('setup.cfg', camera)
-# http://docs.ros.org/api/sensor_msgs/html/msg/CameraInfo.html
-# http://wiki.ros.org/image_pipeline/CameraInfo
-camera_info = CameraInfo()
-camera_info.distortion_model = 'plumb_bob'
-# fill this 3 parameters to get correcty image with stereo camera
-Tx = 0
-Ty = 0
-R = [1, 0, 0, 0, 1, 0, 0, 0, 1]
-camera_info.D = [0]
-camera_info.K = [intrinsic[0][0], intrinsic[0][1], intrinsic[0][2],
-                 intrinsic[1][0], intrinsic[1][1], intrinsic[1][2],
-                 intrinsic[2][0], intrinsic[2][1], intrinsic[2][2]]
-camera_info.R = R
-camera_info.P = [intrinsic[0][0], intrinsic[0][1], intrinsic[0][2], Tx,
-                 intrinsic[1][0], intrinsic[1][1], intrinsic[1][2], Ty,
-                 intrinsic[2][0], intrinsic[2][1], intrinsic[2][2], 0]
+
+with open('camera_info.yml') as f:
+    data = yaml.load(f.read())
+
+del data['roi'], data['header']
+camera_info = CameraInfo(**data)
 
 with rosbag.Bag('%s.bag'%image.header.frame_id, 'w') as bag:
     while 1:
